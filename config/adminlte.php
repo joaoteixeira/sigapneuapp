4a2f199d434bda796ca0d89518f4dac092a1662b
João Teixeira <joao.teixeira@ifro.edu.br>
2018-08-01 23:14:24 -0400

    Config Skin and Name app

--- config/adminlte.php
+++ config/adminlte.php
@@ -42,8 +42,8 @@ return [
     | Menu Logos
     |--------------------------------------------------------------------------
     */
-    'logo_lg'   => '<b>Admin</b>LTE',
-    'logo_mini' => '<b>A</b>LTE',
+    'logo_lg'   => '<b>Siga</b>Pneu App',
+    'logo_mini' => '<b>S</b>Pneu',
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     |
     */
 
-    'credits' => 'Powered By: <a target="_blank" href="mailto:user@example.com">Developer</a>',
+    'credits' => 'Powered By: <a target="_blank" href="http://spacedevapp.com">Siga Pneu Developer</a>',
 
     /*
     |--------------------------------------------------------------------------
@@ -83,5 +83,5 @@ return [
     |
     */
 
-    'theme' => 'skin-blue',
+    'theme' => 'skin-yellow',
 ];
